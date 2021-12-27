825041db3974f8afe876493d0a28ff24b1528d2e
Henrique Rodrigues <hyck.rocha@gmail.com>
2021-12-27 15:36:05 -0300

    Inserção de usuário com POST

--- src/main/java/com/henrique/mongo/resources/UserResource.java
+++ src/main/java/com/henrique/mongo/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.henrique.mongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.henrique.mongo.domain.User;
 import com.henrique.mongo.dto.UserDTO;
@@ -37,5 +40,14 @@ public class UserResource {
 		return ResponseEntity.ok().body(new UserDTO(obj));
 		
 	}
-
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+		
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+		
+	}
 }
--- src/main/java/com/henrique/mongo/services/UserService.java
+++ src/main/java/com/henrique/mongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.henrique.mongo.domain.User;
+import com.henrique.mongo.dto.UserDTO;
 import com.henrique.mongo.repository.UserRepository;
 import com.henrique.mongo.services.exception.ObjectNotFoundException;
 
@@ -29,4 +30,15 @@ public class UserService {
 					
 	}
 	
+	public User insert(User obj) {
+		
+		return repository.insert(obj);
+		
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+		
+	}
 }
